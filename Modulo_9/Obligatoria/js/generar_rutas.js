b67c0e94c75ff9651f55dd7f615481f9b2ac634f
Víctor Sánchez <victorsanchez1993@gmail.com>
2016-03-28 13:37:54 -0430

    Modulo9

--- Modulo_9/Obligatoria/js/generar_rutas.js
+++ Modulo_9/Obligatoria/js/generar_rutas.js
@@ -1,49 +1,5 @@
-$(document).ready(function(){
-    
-     var map, lat, lng;
-
-    $(function(){
-
-      function enlazarMarcador(e){
-
-       // muestra ruta entre marcas anteriores y actuales
-        map.drawRoute({
-          origin: [lat, lng],  // origen en coordenadas anteriores
-          // destino en coordenadas del click o toque actual
-          destination: [e.latLng.lat(), e.latLng.lng()],
-          travelMode: 'driving',
-          strokeColor: '#000000',
-          strokeOpacity: 0.6,
-          strokeWeight: 5
-        });
-
-        lat = e.latLng.lat();   // guarda coords para marca siguiente
-        lng = e.latLng.lng();
-
-        map.addMarker({ lat: lat, lng: lng});  // pone marcador en mapa
-      };
-
-      function geolocalizar(){
-        GMaps.geolocate({
-          success: function(position){
-            lat = position.coords.latitude;  // guarda coords en lat y lng
-            lng = position.coords.longitude;
-
-            map = new GMaps({  // muestra mapa centrado en coords [lat, lng]
-              el: '#map',
-              lat: lat,
-              lng: lng,
-              click: enlazarMarcador,
-              tap: enlazarMarcador
-            });
-            map.addMarker({ lat: lat, lng: lng});  // marcador en [lat, lng]
-          },
-          error: function(error) { alert('Geolocalización falla: '+error.message); },
-          not_supported: function(){ alert("Su navegador no soporta geolocalización"); },
-        });
-      };
-
-      geolocalizar();
-    });
-    
-});
+$(document).ready(function(){var map,lat,lng,i=0,puntoInicial={longitud:0,latitud:0};console.log(puntoInicial);alert("Hola, bienvenido. \n Antes que nada, esta aplicacion funciona utilizando tu Geolocalización por lo cual es importante que le des permiso para acceder a realizar el test. \n Att: Víctor Sánchez");function rutaCompleta(){map.drawRoute({origin:[puntoInicial.latitud,puntoInicial.longitud],destination:[lat,lng],travelMode:'driving',strokeColor:'#000000',strokeOpacity:0.6,strokeWeight:5});map.addMarker({lat:puntoInicial.latitud,lng:puntoInicial.longitud});map.addMarker({lat:lat,lng:lng});}
+function LimpiarMapa(){map.cleanRoute();map.removeMarkers();}
+function enlazarMarcador(e){map.drawRoute({origin:[lat,lng],destination:[e.latLng.lat(),e.latLng.lng()],travelMode:'driving',strokeColor:'#000000',strokeOpacity:0.6,strokeWeight:5});lat=e.latLng.lat();lng=e.latLng.lng();map.addMarker({lat:lat,lng:lng});}
+function geolocalizar(){GMaps.geolocate({success:function(position){lat=position.coords.latitude;lng=position.coords.longitude;map=new GMaps({el:'#map',lat:lat,lng:lng,click:enlazarMarcador,tap:enlazarMarcador});puntoInicial.latitud=lat;puntoInicial.longitud=lng;console.log(puntoInicial);console.log('Te encuentras en: latitud:'+puntoInicial.latitud+' longitud:'+puntoInicial.longitud);map.addMarker({lat:lat,lng:lng});},error:function(error){alert('Geolocalización fallida: '+error.message);},not_supported:function(){alert("Su navegador no soporta Geolocalización.");}});}
+geolocalizar();$("#compactar").on("click",function(){LimpiarMapa();rutaCompleta();setTimeout(alert("Ruta Compactada!. "),10000);});});
